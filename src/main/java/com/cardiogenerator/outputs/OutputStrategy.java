9108aa236e86dd1d029237353bc03b539108fedc
calvinreu <j.calvin.reu@protonmail.com>
2024-05-16 13:25:26 +0200

    add jinit

--- src/main/java/com/cardiogenerator/outputs/OutputStrategy.java
+++ src/main/java/com/cardiogenerator/outputs/OutputStrategy.java
@@ -4,5 +4,5 @@ package com.cardiogenerator.outputs;
  * The OutputStrategy interface defines the output method for alert data.
  */
 public interface OutputStrategy {
-    void output(int patientId, long timestamp, String label, String data);
+  void output(int patientId, long timestamp, String label, String data);
 }
