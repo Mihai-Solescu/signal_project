b59602cb78d412eda0066f04b617f57d1ee1b1cf
Mihai Solescu <mihai.solescu@outlook.com>
2024-06-08 22:48:57 +0200

    alert decorators

--- src/main/java/com/alerts/decorator/PriorityAlertDecorator.java
+++ src/main/java/com/alerts/decorator/PriorityAlertDecorator.java
@@ -3,15 +3,17 @@ package com.alerts.decorator;
 import com.alerts.alert.Alert;
 
 public class PriorityAlertDecorator extends AlertDecorator {
-    private Alert alert;
-    private int priority;
+    private final Alert alert;
+    private final int priority;
 
     public PriorityAlertDecorator(Alert alert, int priority) {
-        super(alert);
-        this.priority = priority;
+      super(alert);
+      this.alert = alert;
+      this.priority = priority;
     }
 
-    public int getPriority() {
-        return priority;
+    @Override
+    public String  getCondition() {
+      return "Priority" + priority + alert.getCondition();
     }
 }
--- src/main/java/com/alerts/decorator/RepeatedAlertDecorator.java
+++ src/main/java/com/alerts/decorator/RepeatedAlertDecorator.java
@@ -3,15 +3,17 @@ package com.alerts.decorator;
 import com.alerts.alert.Alert;
 
 public class RepeatedAlertDecorator extends AlertDecorator {
-    private Alert alert;
-    private int repeatCount;
+    private final Alert alert;
+    private final int repeatCount;
 
     public RepeatedAlertDecorator(Alert alert, int repeatCount) {
         super(alert);
+        this.alert = alert;
         this.repeatCount = repeatCount;
     }
 
-    public int getRepeatCount() {
-        return repeatCount;
+    @Override
+    public String getCondition() {
+        return repeatCount + "x" + alert.getCondition();
     }
 }
